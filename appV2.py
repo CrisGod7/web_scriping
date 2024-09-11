c4244ccfeee05edac69e4fcda61a4f7d4cf7c73b
crisgod <cristian2005barragan@gmaio.com>
2024-09-11 08:41:25 -0600

    v2

--- appV2.py
+++ appV2.py
@@ -13,100 +13,100 @@ from bs4 import BeautifulSoup
 
 class GetProducts:
 
-  def products_amazon(self, url_p, start_page, end_page) -> dict:
-    """
+    def products_amazon(self, url_p, start_page, end_page) -> dict:
+        """
     :param url_p:
     :param start_page:
     :param end_page:
     :return: dict('title', 'price', 'rating')
     """
-    data = {"title": [], "price": [], "rate": []}
-    HEADERS, PROXIES, session = start_requests()
-    """
+        data = {"title": [], "price": [], "rate": []}
+        HEADERS, PROXIES, session = start_requests()
+        """
     Crear un metodo para crear el header, proxies and session. para evitar el bloque de amzaon.
     """
-    url = url_p
-    for i in range(start_page, end_page):
-      print(f'page {i}')
-      response = requests.get(url, headers=HEADERS)
-      if response.status_code != 200:
-        print(f"Error: Unable to fetch page {i}")
-        break
+        url = url_p
+        for i in range(start_page, end_page):
+            print(f'page {i}')
+            response = requests.get(url, headers=HEADERS)
+            if response.status_code != 200:
+                print(f"Error: Unable to fetch page {i}")
+                break
 
-      soup = BeautifulSoup(response.content, 'html.parser')
-      links = soup.find_all("a", attrs={
-        "class": "a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"})
-      links_filters = self.filter_links([link.get('href') for link in links])
+            soup = BeautifulSoup(response.content, 'html.parser')
+            links = soup.find_all("a", attrs={
+                "class": "a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"})
+            links_filters = self.filter_links([link.get('href') for link in links])
 
-      for link in links_filters:
-        print(f"link: {link}")
-        new_webpage = requests.get("https://amazon.com" + link, headers=HEADERS)
-        new_soup = BeautifulSoup(new_webpage.content, 'html.parser')
+            for link in links_filters:
+                print(f"link: {link}")
+                new_webpage = requests.get("https://amazon.com" + link, headers=HEADERS)
+                new_soup = BeautifulSoup(new_webpage.content, 'html.parser')
 
-        data['title'].append(self.get_title(new_soup))
-        data['price'].append(self.get_price(new_soup))
-        data['rate'].append(self.get_rate(new_soup))
+                data['title'].append(self.get_title(new_soup))
+                data['price'].append(self.get_price(new_soup))
+                data['rate'].append(self.get_rate(new_soup))
+                print(data)
+            next_page = get_next_url(url)
+            if not next_page:
+                break
+            url = "https://www.amazon.com" + next_page
 
-      next_page = get_next_url(url)
-      if not next_page:
-        break
-      url = "https://www.amazon.com" + next_page
+        return data
 
-    return data
-
-  @staticmethod
-  def get_title(soup) -> string:
-    """
+    @staticmethod
+    def get_title(soup) -> string:
+        """
     :param soup: new_soup = BeautifulSoup(new_webpage.content, 'html.parser')
     :return:Title
     """
-    try:
-      # <span id="productTitle" class="a-size-large product-title-word-break">        Samsung Galaxy A15 (
-      # SM-155M/DSN), 128GB 6GB RAM, Dual SIM, Factory Unlocked GSM, International
+        try:
+            # <span id="productTitle" class="a-size-large product-title-word-break">        Samsung Galaxy A15 (
+            # SM-155M/DSN), 128GB 6GB RAM, Dual SIM, Factory Unlocked GSM, International
 
-      title = soup.find("span", attrs={"id": "productTitle"})
-      title_value = title.text
-      title_string = title_value.strip()
-    except AttributeError:
-      title_string = ""
-    return title_string
+            title = soup.find("span", attrs={"id": "productTitle"})
+            title_value = title.text
+            title_string = title_value.strip()
+        except AttributeError:
+            title_string = ""
+        return title_string
 
-  @staticmethod
-  def get_price(soup) -> string:
-    """
+    @staticmethod
+    def get_price(soup) -> string:
+        """
     :param soup: new_soup = BeautifulSoup(new_webpage.content, 'html.parser')
     :return: price
     """
-    try:
-      price = soup.find("span", attrs={"class": "a-price-whole"})
-      price_str = price.text
-      price_str = price_str.strip()
-    except AttributeError:
-      price_str = ""
-    return price_str
+        try:
+            price = soup.find("span", attrs={"class": "a-price-whole"})
+            price_str = price.text
+            price_str = price_str.strip()
+        except AttributeError:
+            price_str = ""
+        return price_str
 
-  @staticmethod
-  def get_rate(soup) -> string:
-    """
+    @staticmethod
+    def get_rate(soup) -> string:
+        """
     :param soup: new_soup = BeautifulSoup(new_webpage.content, 'html.parser')
     :return:
     """
-    try:
-      rate = soup.find("span", attrs={"class": "a-icon-alt"})
-      rate_value = rate.text
-      rate_str = rate_value.strip()
-    except AttributeError:
-      rate_str = ""
-    return rate_str
+        try:
+            rate = soup.find("span", attrs={"class": "a-icon-alt"})
+            rate_value = rate.text
+            rate_str = rate_value.strip()
+        except AttributeError:
+            rate_str = ""
+        return rate_str
 
-  @staticmethod
-  def filter_links(links, prefix="/Samsung") -> list:
-    """
+    @staticmethod
+    def filter_links(links, prefix="/Samsung") -> list:
+        """
     :param links:  links = soup.find_all("a", attrs={"class": "a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"})
     :param prefix:  prefix="/Samsung"
     :return: Una nueva lista con los links filtrados.
     """
-    """Filtra una lista de links por un prefijo específico.
+        """Filtra una lista de links por un prefijo específico.
 
       Args:
         links: Una lista de strings representando links.
@@ -120,56 +120,57 @@ class GetProducts:
         #La list comprehension crea una nueva lista con los links que cumplen la condición.
       """
 
-    filtered_links = [link for link in links if re.match(f"^{prefix}", link, re.IGNORECASE)]
-    return filtered_links
+        filtered_links = [link for link in links if re.match(f"^{prefix}", link, re.IGNORECASE)]
+        return filtered_links
 
 
 def start_requests():
-  delay_request()
-  session = requests.Session()
-  session.headers.update({
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
-    'Accept-Language': 'en-US,en;q=0.5'
-  })
-  USER_AGENTS = [
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
-    # Añade más User-Agents aquí
-  ]
-  HEADERS = {
-    'User-Agent': random.choice(USER_AGENTS),
-    'Accept-Language': 'en-US,en;q=0.5'
-  }
-  PROXIES = {
-    'http': 'http://10.10.1.10:3128',
-    'https': 'http://10.10.1.10:1080',
-    # Añade más proxies aquí
-  }
-  return HEADERS, PROXIES, session
+    delay_request()
+    session = requests.Session()
+    session.headers.update({
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
+        'Accept-Language': 'en-US,en;q=0.5'
+    })
+    USER_AGENTS = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+        # Añade más User-Agents aquí
+    ]
+    HEADERS = {
+        'User-Agent': random.choice(USER_AGENTS),
+        'Accept-Language': 'en-US,en;q=0.5'
+    }
+    PROXIES = {
+        'http': 'http://10.10.1.10:3128',
+        'https': 'http://10.10.1.10:1080',
+        # Añade más proxies aquí
+    }
+    return HEADERS, PROXIES, session
 
 
 def get_next_url(url_actual):
-  HEADERS, PROXIES, session = start_requests()
+    HEADERS, PROXIES, session = start_requests()
 
-  try:
-    response = requests.get(url_actual, headers=HEADERS)
-    response.raise_for_status()
+    try:
+        response = requests.get(url_actual, headers=HEADERS)
+        response.raise_for_status()
 
-    soup = BeautifulSoup(response.content, 'html.parser')
-    next_url = soup.find('a', {'class': 's-pagination-next'})
+        soup = BeautifulSoup(response.content, 'html.parser')
+        next_url = soup.find('a', {'class': 's-pagination-next'})
 
-    if next_url and 'href' in next_url.attrs:
-      return next_url['href']
-    else:
-      return None
+        if next_url and 'href' in next_url.attrs:
+            return next_url['href']
+        else:
+            return None
 
-  except requests.exceptions.RequestException as e:
-    print(f"Error al obtener la página: {e}")
-  return None
+    except requests.exceptions.RequestException as e:
+        print(f"Error al obtener la página: {e}")
+    return None
+
 
 def delay_request():
-  """
+    """
   Creamos un delay para evitar ser detectados como un bot
   :return:
   """
-  time.sleep(random.uniform(1, 3))  # Espera entre 1 y 3 segundos
+    time.sleep(random.uniform(1, 3))  # Espera entre 1 y 3 segundos
--- frame.py
+++ frame.py
@@ -4,16 +4,17 @@ import appV2
 import pandas as pd
 
 
-def diccionario_a_csv_pandas(diccionario, nombre_archivo):
-    df = pd.DataFrame([diccionario])  # Crear un DataFrame con una sola fila
-    df.to_csv(nombre_archivo, index=False)  # Guardar como CSV sin índice
 
 
 url = "https://www.amazon.com/s?k=samsung&ref=nb_sb_noss"
 product = appV2.GetProducts()
-dic_data = product.products_amazon(url, start_page=0, end_page=4)
+dic_data = product.products_amazon(url, start_page=0, end_page=1)
+print(dic_data)
+
 # Crear un DataFrame a partir del StringIO
 
-diccionario_a_csv_pandas(dic_data, "Product_app.csv")
 
-df = pd.read_csv("Product_app.csv")
+df = pd.read_cv("product_app.csv")
+df_2 = pd.DataFrame(dic_data)
+df_2.to_csv("product2_app.csv", index=False)
+print(df_2)
